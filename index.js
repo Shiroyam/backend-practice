720cf5d20574dcfc4d866f01d9e241bafbd5f272
Semyon Zasov <semenmaslina@gmail.com>
2022-04-20 15:20:55 +0300

    add scheam DB

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 import express from "express";
 import mongoose from "mongoose";
+import Post from "./Post.js";
 
 const PORT = 5000;
-const DB_URL = "mongodb+srv://user:user@cluster0.lwikw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const DB_URL =
+  "mongodb+srv://user:user@cluster0.lwikw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).json("Hello world!")
-})
+app.post("/", async (req, res) => {
+  try {
+    const { author, title, content, picture } = req.body;
+    const post = await Post.create({ author, title, content, picture });
+    res.json(post);
+  } catch (error) {
+    res.status(500).json(error)
+  }
+});
 
-async function startApp (){
-    try {
-        await mongoose.connect(DB_URL)
-        app.listen(PORT, ()=> console.log("server working" + PORT))
-    } catch (error) {
-        console.log(error)
-    }
+async function startApp() {
+  try {
+    await mongoose.connect(DB_URL);
+    app.listen(PORT, () => console.log("server working" + PORT));
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-startApp()
-
+startApp();

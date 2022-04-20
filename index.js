693f44bbbf31a59abcb602151e77c041c48c20f9
Semyon Zasov <semenmaslina@gmail.com>
2022-04-20 14:30:36 +0300

    start server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("1")
+import express from "express";
+
+const PORT = 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json("Hello world!")
+})
+
+app.listen(PORT, ()=> console.log("server working" + PORT))

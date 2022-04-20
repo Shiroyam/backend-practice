34cc4ea8633307ae04ad6e509831d173030c5684
Semyon Zasov <semenmaslina@gmail.com>
2022-04-20 15:09:15 +0300

    add datebase

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const PORT = 5000;
+const DB_URL = "mongodb+srv://user:user@cluster0.lwikw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 const app = express();
 
@@ -8,4 +10,14 @@ app.get('/', (req, res) => {
     res.status(200).json("Hello world!")
 })
 
-app.listen(PORT, ()=> console.log("server working" + PORT))
+async function startApp (){
+    try {
+        await mongoose.connect(DB_URL)
+        app.listen(PORT, ()=> console.log("server working" + PORT))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+startApp()
+
